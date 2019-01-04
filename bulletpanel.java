61cdc6f49cbbeb0ea0bab06840c9a9fbce2cde54
JamesCahyadi <45665593+JamesCahyadi@users.noreply.github.com>
2019-01-04 14:11:34 -0500

    Drew CSV file map

--- bulletpanel.java
+++ bulletpanel.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import javax.imageio.*;
-import java.util.Random;
+import java.awt.image.*;
 
 public class bulletpanel extends JPanel{
 	
@@ -19,14 +19,105 @@ public class bulletpanel extends JPanel{
 	double dblRun;
 	int intSpeed=2;
 	
+	BufferedImage wood;
+	BufferedImage sky;
+	int intRow;
+	int intColumn;
+	String strMap[][];
+	String strSplit[];
+	String strLine = "";
+	boolean blnMapFail = false;
+	FileReader map = null;
+	BufferedReader mapdata = null;
+	
+	int intPlayerX = 0;
+	int intPlayerY = 330;
+	int intPlayerWidth = 20;
+	int intPlayerHeight = 30;
+	int intPlayerSpeed = 20;
+	boolean blnPlayerRight = false;
+	boolean blnPlayerLeft = false;
+	boolean blnPlayerUp = false;
+	boolean blnPlayerDown = false;
+	
+	boolean blnReadMap = false;
+		
 	public void paintComponent(Graphics g){
-		g.setColor(Color.white);
-		g.fillRect(0,0,1280,720);
-		g.setColor(Color.red);
-		intBulletX = (int)dblBulletX;
-		intBulletY = (int)dblBulletY;
-		g.fillOval(intBulletX,intBulletY,10,10);
-		if(blnFire=true){
+		
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, 1280, 800); 
+		
+		if(blnReadMap == false){
+			strMap = new String[18][32];
+			
+			//Read map.csv file
+			try{
+				map = new FileReader("map.csv");
+			}catch(FileNotFoundException e){
+				System.out.println("Error! Could not find map file.");
+				blnMapFail = true;
+			}
+			
+			// Map data can only be read if map file is found
+			if(blnMapFail == false){
+				mapdata = new BufferedReader(map);
+			}
+
+			for(intRow = 0; intRow < 18; intRow++){ 
+				try{ 
+					strLine = mapdata.readLine();
+				}catch(IOException e){
+					System.out.println("Can't read line");
+				}
+				strSplit = strLine.split(",");  
+				for(intColumn = 0; intColumn < 32; intColumn++){ 
+					strMap[intRow][intColumn] = strSplit [intColumn];   
+				}	
+			}
+		}
+		blnReadMap = true;
+		for(intRow = 0; intRow < 18; intRow++){
+			for(intColumn = 0; intColumn < 32; intColumn++){
+				if(strMap[intRow][intColumn].equalsIgnoreCase("g")){ // Draw ground
+					try{
+						wood = ImageIO.read(new File("wood.jpg"));
+						g.drawImage(wood, (intColumn*40), (intRow*40), null);
+					}catch(IOException e){
+						System.out.println("Unable to load wood image");
+					}
+					
+				}else if(strMap[intRow][intColumn].equalsIgnoreCase("s")){ // Draw sky
+					try{
+						sky = ImageIO.read(new File("water.jpg"));
+						g.drawImage(sky, (intColumn*40), (intRow*40), null);
+					}catch(IOException e){
+						System.out.println("Unable to load sky image");
+					}
+				}
+			}
+		}
+		
+
+		// Player 
+		g.setColor(Color.blue);
+		g.fillRect(intPlayerX, intPlayerY, intPlayerWidth, intPlayerHeight);
+		
+		if(blnPlayerRight){
+			intPlayerX = intPlayerX + intPlayerSpeed;
+		}
+		if(blnPlayerLeft){
+			intPlayerX = intPlayerX - intPlayerSpeed;
+		}
+		if(blnPlayerUp){
+			intPlayerY = intPlayerY - intPlayerSpeed;
+		}
+		
+		// Bullet
+		if(blnFire){
+			g.setColor(Color.red);
+			intBulletX = (int)dblBulletX;
+			intBulletY = (int)dblBulletY;
+			g.fillOval(intBulletX,intBulletY,10,10);
 			if(blnGetSlope){
 				/*
 				 * Imaginary Triangle: 
@@ -41,17 +132,14 @@ public class bulletpanel extends JPanel{
 				dblRise = intSpeed*Math.sin(dblAngle);
 				dblRun = intSpeed*Math.cos(dblAngle);
 				blnGetSlope=false; //Only get slope again when the next click happens
-			}
+			}	
 			
 			dblBulletX+=dblRun;
 			dblBulletY+=dblRise;
-			
-		}
-		
+		}		
 	}
 
 	public bulletpanel(){
 		super();
 	}
-	
 }
--- bullettest.java
+++ bullettest.java
@@ -1,13 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.*;
+import java.awt.image.BufferedImage;
 
-public class bullettest implements ActionListener, MouseListener{
+public class bullettest implements ActionListener, MouseListener, KeyListener{
 	JFrame frame;
 	bulletpanel panel;
 	Timer timer;
-	
-                   
+	                   
 	public void actionPerformed(ActionEvent evt){
 		if(evt.getSource() == timer){
 			panel.repaint();
@@ -34,30 +37,54 @@ public class bullettest implements ActionListener, MouseListener{
 	}
 	public void mouseClicked(MouseEvent evt){}
 	
-
+	public void keyReleased(KeyEvent evt){
+		if(evt.getKeyCode() == 37){
+			panel.blnPlayerLeft = false;
+		}else if(evt.getKeyCode() == 38){
+			panel.blnPlayerUp = false;
+		}else if(evt.getKeyCode() == 39){
+			panel.blnPlayerRight = false;
+		}else if(evt.getKeyCode() == 40){
+			panel.blnPlayerDown = false;
+		}	
+	}
 	
+	public void keyPressed(KeyEvent evt){
+		if(evt.getKeyCode() == 37){
+			panel.blnPlayerLeft = true;
+		}else if(evt.getKeyCode() == 38){
+			panel.blnPlayerUp = true;
+		}else if(evt.getKeyCode() == 39){
+			panel.blnPlayerRight = true;
+		}else if(evt.getKeyCode() == 40){
+			panel.blnPlayerDown = true;
+		}
+	}
+
+	public void keyTyped(KeyEvent evt){}
+
+		
 	public bullettest(){
 		frame = new JFrame("bullet");
 		panel = new bulletpanel();
 		panel.setLayout(null);
 		panel.setPreferredSize(new Dimension(1280,720));
-		panel.addMouseListener(this);
+
 		
 		frame.setContentPane(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
+		frame.setResizable(false); 
 		frame.setVisible(true);
+		
+		frame.addKeyListener(this);
+		panel.addMouseListener(this);
 		
 		timer = new Timer(1000/60, this);
 		timer.start();
-		
 	}
 	
 	public static void main(String[] args){
-		new bullettest();
-		String strMap[][];
-		strMap = new String[32][18];
-		
-		
+		new bullettest();	
 	}
 }
