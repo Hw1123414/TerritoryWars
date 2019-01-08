1b7c5a551695ae4eae03d010702ea2974c7a430a
rayc4 <45674759+rayc4@users.noreply.github.com>
2019-01-07 19:44:37 -0500

    Combined Networking and main file

---
+++ TerritoryWars.java
@@ -0,0 +1,215 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TerritoryWars implements ActionListener, MouseListener, MouseMotionListener, KeyListener{
+	// Networking Variables
+	String strName;
+	boolean blnHost=true;
+	
+	// Properties
+	JFrame frame = new JFrame("Territory Wars");
+	TerritoryWarsPanel panel = new TerritoryWarsPanel();
+	JTextArea area = new JTextArea();
+	JScrollPane scroll = new JScrollPane(area);
+	JTextField field = new JTextField();
+	JTextField inputIP = new JTextField();
+	SuperSocketMaster ssm;
+	JButton host = new JButton("Host Server");
+	JButton client = new JButton("Join Server");
+	JLabel hostIP = new JLabel("host");
+	JButton OK = new JButton("OK");
+	JLabel entername = new JLabel("Your Name:");
+	JTextField namefield = new JTextField();
+	JLabel enterhostIP = new JLabel("Host IP:");
+	JButton start = new JButton("Start Game");
+	Timer timer;
+	
+	
+	public void actionPerformed(ActionEvent evt){
+		if(evt.getSource() == timer){
+			panel.repaint();
+		}
+		// Host button Clicked
+		if(evt.getSource()==host || evt.getSource()==client){
+			if(evt.getSource()==client){
+				blnHost=false;
+				// Show IP input field
+				enterhostIP.setSize(300,25);
+				enterhostIP.setLocation(420,500);
+				panel.add(enterhostIP);
+				inputIP.setSize(300,25);
+				inputIP.setLocation(500,500);
+				inputIP.addActionListener(this);
+				panel.add(inputIP);
+			}
+			
+			// Remove Buttons
+			panel.remove(host);
+			panel.remove(client);
+			panel.validate();
+			panel.repaint();
+			
+			// Ask for name
+			entername.setSize(100,25);
+			entername.setLocation(410,550);
+			panel.add(entername);
+			namefield.setSize(300,25);
+			namefield.setLocation(500,550);
+			namefield.addActionListener(this);
+			panel.add(namefield);
+			
+			// OK Button
+			OK.setSize(100,50);
+			OK.setLocation(580, 600);
+			OK.addActionListener(this);
+			panel.add(OK);
+		}
+		
+		// 'OK' button pressed
+		if(evt.getSource()==OK){
+			// Get username
+			strName = namefield.getText();
+			// Host
+			if(blnHost){
+				// Show IP
+				hostIP.setSize(400,50);
+				hostIP.setLocation(500,500);
+				panel.add(hostIP);
+				ssm = new SuperSocketMaster(6112, this);
+				ssm.connect();
+				hostIP.setText("Started server, your IP address is: "+ssm.getMyAddress());
+			// Client
+			}else{
+				ssm = new SuperSocketMaster(inputIP.getText(),6112,this);
+				ssm.connect();
+			}
+			panel.remove(OK);
+			panel.remove(namefield);
+			panel.remove(entername);
+			// Show 'Start Game' button
+			start.setSize(100,50);
+			start.setLocation(580,600);
+			start.addActionListener(this);
+			panel.add(start);
+		}
+		if(evt.getSource()==start){
+			panel.remove(hostIP);
+			panel.remove(start);
+			panel.remove(inputIP);
+			panel.blnStartGame=true;
+		}
+		
+		//Chat
+		if(evt.getSource()==field){
+			ssm.sendText(strName+": "+field.getText());
+			area.append(strName+": "+field.getText()+"\n");
+			field.setText("");
+		}else if(evt.getSource()==ssm){
+			String strData = ssm.readText();
+			area.append(strData+"\n");
+		}
+	}
+	
+	// MouseListener
+	public void mouseExited(MouseEvent evt){}
+	public void mouseEntered(MouseEvent evt){}
+	public void mouseReleased(MouseEvent evt){}
+	
+	// When mouse is clicked
+	public void mousePressed(MouseEvent evt){
+		if(evt.getX()>0 && evt.getX()<1280 && evt.getY()>0 && evt.getY()<720){
+			panel.dblBulletX=panel.dblPlayerX;
+			panel.dblBulletY=panel.dblPlayerY;
+
+			panel.dblMouseX=evt.getX();
+			panel.dblMouseY=evt.getY();
+			panel.blnFire=true;
+			panel.blnGetSlope=true;
+		}
+	}
+
+	// MouseMotionListener
+	public void mouseMoved(MouseEvent evt){
+		panel.dblMouseX=evt.getX();
+		panel.dblMouseY=evt.getY();	
+	}
+	public void mouseDragged(MouseEvent evt){}
+	public void mouseClicked(MouseEvent evt){}
+	
+	// KeyListener
+	public void keyReleased(KeyEvent evt){
+		switch(evt.getKeyCode()){
+			case 37: panel.blnPlayerLeft=false;
+				break;
+			case 38: panel.blnPlayerUp=false;
+				break;
+			case 39: panel.blnPlayerRight=false;
+				break;
+		}
+	}
+	
+	public void keyPressed(KeyEvent evt){
+		System.out.println("Test");
+		switch(evt.getKeyCode()){
+			case 37: panel.blnPlayerLeft=true;
+				break;
+			case 38: panel.blnPlayerUp=true;
+				break;
+			case 39: panel.blnPlayerRight=true;
+				break;	
+		}
+		// Chat box
+		if(panel.blnStartGame && evt.getKeyCode()==10){
+			scroll.setSize(400,400);
+			scroll.setLocation(0,0);
+			panel.add(scroll);
+			area.setEnabled(false);
+			
+			field.setSize(400,100);
+			field.setLocation(0,400);
+			field.addActionListener(this);
+			panel.add(field);
+		}
+	}
+	public void keyTyped(KeyEvent evt){}
+	
+	// Constructor
+	public TerritoryWars(){
+		panel.setLayout(null);
+		panel.setPreferredSize(new Dimension(1280,720));
+		
+		host.setSize(100,50);
+		host.setLocation(500,500);
+		host.addActionListener(this);
+		panel.add(host);
+		
+		client.setSize(100,50);
+		client.setLocation(700,500);
+		client.addActionListener(this);
+		panel.add(client);
+
+		panel.setLayout(null);
+		panel.setPreferredSize(new Dimension(1280,720));
+		
+		panel.addMouseListener(this);
+		panel.addMouseMotionListener(this);
+		panel.setFocusable(true);
+        panel.requestFocus();
+		panel.addKeyListener(this);
+		
+		frame.setResizable(false);
+		frame.setContentPane(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		
+		timer = new Timer(1000/60, this);
+		timer.start();
+	}
+	
+	// Main method
+	public static void main(String[] args){
+		new TerritoryWars();
+	}
+}
--- TerritoryWarsPanel.java
+++ TerritoryWarsPanel.java
@@ -1,130 +1,179 @@
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.*;
 
-public class TerritoryWarsPanel implements ActionListener{
-	// Variables
-	String strName;
-	boolean blnHost=true;
+public class TerritoryWarsPanel extends JPanel{
 	
-	// Properties
-	JFrame frame = new JFrame("Territory Wars");
-	JPanel panel = new JPanel();
-	JTextArea area = new JTextArea();
-	JScrollPane scroll = new JScrollPane(area);
-	JTextField field = new JTextField();
-	JTextField inputIP = new JTextField();
-	SuperSocketMaster ssm;
-	JButton host = new JButton("Host Server");
-	JButton client = new JButton("Join Server");
-	JLabel hostIP = new JLabel("host");
-	JButton enterbutton = new JButton("Enter");
-	JLabel entername = new JLabel("Your Name:");
-	JTextField namefield = new JTextField();
-	JLabel enterhostIP = new JLabel("Host IP:");
+// Variables
+	// General
+	double dblMouseX;
+	double dblMouseY;
+	double dblPlayerX=0;
+	double dblPlayerY=100;
 	
-	// Methods
-	public void actionPerformed(ActionEvent evt){
-		// Host button Clicked
-		if(evt.getSource()==host || evt.getSource()==client){
-			if(evt.getSource()==client){
-				blnHost=false;
-				// Show IP input field
-				enterhostIP.setSize(300,25);
-				enterhostIP.setLocation(420,500);
-				panel.add(enterhostIP);
-				inputIP.setSize(300,25);
-				inputIP.setLocation(500,500);
-				inputIP.addActionListener(this);
-				panel.add(inputIP);
+	// Bullet Travel
+	boolean blnFire=false;
+	double dblBulletX;
+	double dblBulletY;
+	double dblBulletAngle; //Angle in radians
+	boolean blnGetSlope=true; //to get the slope once per click only
+	double dblBulletRise;
+	double dblBulletRun;
+	int intSpeed=4;
+	
+	// Laser
+	double dblLaserAngle;
+	double dblLaserLength=50;
+	double dblLaserRise;
+	double dblLaserRun;
+	double dblLaserX;
+	double dblLaserY;
+	
+	// Grenade
+	double dblPower=5;
+	
+	// Map
+	BufferedImage wood;
+	BufferedImage sky;
+	int intRow;
+	int intColumn;
+	String strMap[][];
+	String strSplit[];
+	String strLine = "";
+	FileReader map = null;
+	BufferedReader mapdata = null;
+		
+	// Character Movement
+	int intPlayerWidth = 20;
+	int intPlayerHeight = 30;
+	int intPlayerSpeed = 20;
+	boolean blnPlayerRight = false;
+	boolean blnPlayerLeft = false;
+	boolean blnPlayerUp = false;
+	boolean blnPlayerDown = false;
+	
+	boolean blnStartGame=false;
+	
+	// Graphics
+	public void paintComponent(Graphics g){	
+		if(blnStartGame){	
+			//Draw Map
+			for(intRow = 0; intRow < 18; intRow++){
+				for(intColumn = 0; intColumn < 32; intColumn++){
+					if(strMap[intRow][intColumn].equals("g")){ // Draw ground
+						g.drawImage(wood, (intColumn*40), (intRow*40), null);
+					}else if(strMap[intRow][intColumn].equals("s")){ // Draw sky
+						g.drawImage(sky, (intColumn*40), (intRow*40), null);
+					
+					}
+				}
 			}
 			
-			// Remove Buttons
-			panel.remove(host);
-			panel.remove(client);
-			panel.validate();
-			panel.repaint();
-			
-			// Ask for name
-			entername.setSize(100,25);
-			entername.setLocation(410,550);
-			panel.add(entername);
-			namefield.setSize(300,25);
-			namefield.setLocation(500,550);
-			namefield.addActionListener(this);
-			panel.add(namefield);
-			
-			// Enter Button
-			enterbutton.setSize(100,50);
-			enterbutton.setLocation(570, 600);
-			enterbutton.addActionListener(this);
-			panel.add(enterbutton);
-		}
-		
-		// Enter button pressed
-		if(evt.getSource()==enterbutton){
-			strName = namefield.getText();
-			if(blnHost){
-				// Show IP
-				hostIP.setSize(400,50);
-				hostIP.setLocation(500,500);
-				panel.add(hostIP);
-				ssm = new SuperSocketMaster(6112, this);
-				ssm.connect();
-				hostIP.setText("Started server, your IP address is: "+ssm.getMyAddress());
-				System.out.println(ssm.getMyHostname());
-			}else{
-				ssm = new SuperSocketMaster(inputIP.getText(),6112,this);
-				ssm.connect();
+			// Player 
+			g.setColor(Color.blue);
+			g.fillRect((int)Math.round(dblPlayerX), (int)Math.round(dblPlayerY), intPlayerWidth, intPlayerHeight);
+			if(blnPlayerRight){
+				dblPlayerX+=intPlayerSpeed;
 			}
-		}
-		
-		//Chat
-		if(evt.getSource()==field){
-			ssm.sendText(strName+": "+field.getText());
-			area.append(strName+": "+field.getText()+"\n");
-			field.setText("");
-		}else if(evt.getSource()==ssm){
-			String strData = ssm.readText();
-			area.append(strData+"\n");
+			if(blnPlayerLeft){
+				dblPlayerX-=intPlayerSpeed;
+			}
+			if(blnPlayerUp){
+				dblPlayerY-=intPlayerSpeed;
+			}
+			if(blnPlayerDown){
+				dblPlayerY+=intPlayerSpeed;			
+			}
+			
+		// Draw laser (constant length of dblLaserLength)
+			g.setColor(Color.red);
+			// Find angle
+			dblLaserAngle = Math.atan2(dblMouseY-dblPlayerY, dblMouseX-dblPlayerX);
+			// Find rise and run using trig
+			dblLaserRise = dblLaserLength*Math.sin(dblLaserAngle);
+			dblLaserRun = dblLaserLength*Math.cos(dblLaserAngle);
+			// Find coordinates of the laser's endpoint
+			dblLaserX = dblPlayerX+dblLaserRun;
+			dblLaserY = dblPlayerY+dblLaserRise;
+			// Draw
+			g.drawLine((int)Math.round(dblPlayerX),(int)Math.round(dblPlayerY),
+			(int)Math.round(dblLaserX),(int)Math.round(dblLaserY));
+			
+			// Fire bullet
+			g.fillOval((int)Math.round(dblBulletX)-5,(int)Math.round(dblBulletY)-5,10,10);
+			if(blnFire){
+				/* Line
+				if(blnGetSlope){
+					/*
+					 * Angle between horizontal and the imaginary line (player to cursor) is found using arctangent
+					 * Rise is found using sine
+					 * Run is found using cosine
+					 * Run and rise are added to the x and y values respectively for every frame
+					 * This method ensures that the bullet will travel at the same speed at all angles
+					 * hypotenuse (intSpeed) is constant for any slope
+					 *
+					dblBulletAngle = Math.atan2(dblMouseY-dblBulletY, dblMouseX-dblBulletX);
+					dblBulletRise = intSpeed*Math.sin(dblBulletAngle);
+					dblBulletRun = intSpeed*Math.cos(dblBulletAngle);
+					blnGetSlope=false; //Only get slope again when the next click happens
+				}
+				dblBulletX+=dblBulletRun;
+				dblBulletY+=dblBulletRise;
+				*/
+				 
+				if(blnGetSlope){
+					dblBulletAngle = Math.atan2(dblMouseY-dblPlayerY, dblMouseX-dblPlayerX);
+					dblBulletRise = -dblPower*Math.sin(dblBulletAngle);
+					dblBulletRun = dblPower*Math.cos(dblBulletAngle);
+					blnGetSlope=false;
+				}
+				dblBulletX+=dblBulletRun;
+				dblBulletY-=dblBulletRise;
+				dblBulletRise-=0.05;		
+			}
 		}
 	}
-	
-	// Constructors
+
 	public TerritoryWarsPanel(){
-		panel.setLayout(null);
-		panel.setPreferredSize(new Dimension(1280,720));
+		super();
 		
-		host.setSize(100,50);
-		host.setLocation(500,500);
-		host.addActionListener(this);
-		panel.add(host);
+		// Load map csv
+		strMap = new String[18][32];
 		
-		client.setSize(100,50);
-		client.setLocation(700,500);
-		client.addActionListener(this);
-		panel.add(client);
+		try{
+			map = new FileReader("map.csv");
+		}catch(FileNotFoundException e){
+			System.out.println("Error! Could not find map file.");
+		}
 		
-		scroll.setSize(400,400);
-		scroll.setLocation(0,0);
-		panel.add(scroll);
-		
-		field.setSize(400,100);
-		field.setLocation(0,400);
-		field.addActionListener(this);
-		panel.add(field);
+		mapdata = new BufferedReader(map);
 
-		frame.setResizable(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(panel);
-		frame.pack();
-		frame.setVisible(true);
+		for(intRow = 0; intRow < 18; intRow++){ 
+			try{ 
+				strLine = mapdata.readLine();
+			}catch(IOException e){
+				System.out.println("Can't read line");
+			}
+			strSplit = strLine.split(",");  
+			for(intColumn = 0; intColumn < 32; intColumn++){ 
+				strMap[intRow][intColumn] = strSplit [intColumn];   
+			}	
+		}
 		
-		
+		// import map images
+		try{
+			wood = ImageIO.read(new File("wood.jpg"));		
+		}catch(IOException e){
+			System.out.println("Unable to load wood image");
+		}
+					
+		try{
+			sky = ImageIO.read(new File("water.jpg"));	
+		}catch(IOException e){
+			System.out.println("Unable to load sky image");
+		}
 	}
 	
-	// Main Method
-	public static void main(String[] args){
-		new TerritoryWarsPanel();
-	}
 }
